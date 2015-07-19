29752a777809e8b079e197fb89760a49331f7b3b
Niv Levy <niv@valis>
2015-07-18 22:09:20 -0700

    Calculate airspeed and send airspeed, pressure and vario to FC using openvario protocol - which i'm not sure if working (or maybe somehting else is wrong). In any case, i can connect to the nook and it accepts the gps. Connected to tophat (nook) on Serial3 at 4800 baud, sending air data and latest GPRMC message once a second. Still  sending same info on usb serial for debugging - doesn't seem to interfere with anything.

--- vario_v0a.ino
+++ vario_v0a.ino
@@ -3,9 +3,12 @@
 // released under GPL v2
 
 
-// TODO implement the openvario protocol - follow https://github.com/Turbo87/openvario-protocol
+// Using openvario protocol - follow https://github.com/Turbo87/openvario-protocol (which tophat beta doesn't seem to actually get? may be worthwhile to implement something else
 
 // originally based on http://www.rcgroups.com/forums/showthread.php?t=1749208  ( Rolf R Bakke, Oct 2012), but zero remains.
+
+#define USE_STEPPER 1
+
 
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
@@ -20,15 +23,20 @@
 #define DPducer_PIN 14
 ADC *adc = new ADC(); // adc object;
 
+
+#ifdef USE_STEPPER
 // change this to the number of steps on your motor
 #define STEPS 100
 // create an instance of the stepper class, specifying
 // the number of steps of the motor and the pins it's
 // attached to
-Stepper stepper(STEPS, 4, 5, 6, 7);
+// was 4,5,6,7 moving to 3,4,5,6 to free 7 and 8 for serial port
+Stepper stepper(STEPS, 3, 4, 5, 6);
     
 // how many steps per m/sec of climb rate (or something - units are still uncertain at this point in time. assuming we want a 20kt range (+-10kt) = 10m/sec range over 600 steps -> 1m/sec is 60 steps
 #define STEP_MULT 60.0 // 
+
+#endif //USE_STEPPER
 
 // i'm going to do the filtering not so nicely, by just implementing a ring buffer, storing deltas and going from there. Since bmp180 seems to get about 30Hz, and i'd like at most 0.5sec, we'll start with 20 samples.
 // we should at least use a non square window, TODO
@@ -56,9 +64,12 @@ elapsedMillis t = 0;
 
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(BMP085_MODE_ULTRAHIGHRES);
 
+// gps connected to Serial1
 TinyGPS gps;
 #define Uart Serial1
 
+// FC connected on pins 7,8; pin8 is tx 
+#define FC Serial3
 
 void time_isr(void) {
     since_last = long(t);
@@ -77,6 +88,9 @@ float ddsAcc; // int is 4 bytes on the later arduino and teensy!
 short position, old_position;
 
 float dp_offset;
+
+// keep track of gps message and just copy it when it's done. we don't actually need the parser, but i'll use it to see when a message is finished!
+String gps_s = String();
 
 // borrowed from http://jwbrooks.blogspot.com/2014/02/arduino-linear-regression-function.html
 void simpLinReg(float* x, float* y, float* lrCoef, int n){
@@ -153,6 +167,8 @@ const float Vcc = 5.0;//(NOTE: we could measure this using an a2d channel and a 
 // calculate true airspeed based on pressure and differential pressure.
 // following densiy model to 11Km
 float TAS(float altitude) {
+    float EAS, volts, q, roe, TAS;
+    int value;
     // from wikipedia 
     // TAS = EAS * sqrt(roe_0 / roe)
     // EAS = sqrt(2*q/ roe_0)
@@ -164,20 +180,67 @@ float TAS(float altitude) {
     
     //read diff presssure directly (nothing else needs it)
     //dp_offset : signal at P=0 -vs/4
-    int value = adc->analogRead(DPducer_PIN); // = Vout / 2 = Vs/2 * (0.057*P + 0.5) + error/2
-    float volts = value * 3.3 / adc->getMaxValue(ADC_0);
+    value = adc->analogRead(DPducer_PIN); // = Vout / 2 = Vs/2 * (0.057*P + 0.5) + error/2
+    volts = value * 3.3 / adc->getMaxValue(ADC_0);
     //Serial.println(volts);
-    float q = ((volts - dp_offset) * 2.0/ Vcc  - 0.5) / 0.057; //kPa
+    q = ((volts - dp_offset) * 2.0/ Vcc  - 0.5) / 0.057; //kPa
     //Serial.println(q);
-    float EAS = sqrt(2*abs(q) * 1000.0 / roe_0); // m/sec
+    // TODO: i'm not sure what is a legit thing to do - i can think of cases where pitot is lower than static, but whether to treat them as real airspeed or just throw them out is unclear to me. (i.e. it's essentially something/one sucking on the pitot - but i can think of a transient during e.g. a tail slide that would do this)
+    if (q>0)
+	EAS = sqrt(2*q * 1000.0 / roe_0); // m/sec
+    else
+	EAS = -sqrt(2*(-q) * 1000.0 / roe_0); //m/sec
     //https://en.wikipedia.org/wiki/Barometric_formula
     
-    float roe = roe_0 * pow((1- L_b * altitude/Tb), 1 + g * M / (R -L_b));
-    float TAS = EAS * sqrt(roe_0/roe);
+    roe = roe_0 * pow((1- L_b * altitude/Tb), 1 + g * M / (R -L_b));
+    TAS = EAS * sqrt(roe_0/roe);
     return TAS;
     //Serial.println(EAS);
     //return EAS;
 }    
+
+
+// send a single POV sentence
+void send_POV(float x, char t) {
+    byte checksum = 0;
+    unsigned int i;
+    int N, frac;
+    float abs_x = abs(x);
+    String ex_zero = String(), s = String(), ps = String();
+    
+    N = floor(abs_x);
+    frac = floor((abs_x - N) * 100); // need two decimal places
+    if (frac < 10)
+	ex_zero += "0";
+
+    if (x >=0)
+	ps = ps + "POV," + t + "," + N + "." + ex_zero + frac;
+	//s = s + "$POV," + t + "," + N + "." + ex_zero + frac + "*" + checksum;
+    else
+	//s = s + "$POV," + t + ",-" + N + "." + ex_zero + frac + "*" + checksum;
+	ps = ps + "POV," + t + ",-" + N + "." + ex_zero + frac;
+    // checksum calculation from https://rietman.wordpress.com/2008/09/25/how-to-calculate-the-nmea-checksum/
+    for (i=0; i< ps.length(); i++)
+	checksum ^= ps[i]; 
+    s = s + "$" + ps + "*";
+    Serial.print(s);
+    Serial.println(checksum, HEX);
+    FC.print(s);
+    FC.println(checksum, HEX);
+    return;
+}
+
+// send information to flight computer (tophat, xcsoar..) using openvario spec, as defined at
+// https://github.com/Turbo87/openvario-protocol
+//$POV,<type>,<value>,<type>,<value>,...*<checksum>
+void info2FC(float airspeed, float climb_rate, float pressure) {
+    send_POV(airspeed * 3.6, 'S'); // convert to km/h
+    send_POV(climb_rate, 'E');  // already in m/sec
+    send_POV(pressure / 100.0, 'P');  //already in hPa
+    //TODO send gps! we want to send RMC, GGA, GSA and GSV (and probbaly less?)
+    return;
+}
+
 
 void setup()
 {
@@ -221,17 +284,21 @@ void setup()
     }
     dp_offset = dp_offset / 200.0;
     
+#ifdef USE_STEPPER
     stepper.setSpeed(300);
     // try to get it to center
     // uncomment this if necessary to set - but it does bang the motor against the stop!
     stepper.step(-600);
     stepper.step(300);
     old_position = position = 300;
+#endif //USE_STEPPER
     
     Timer1.initialize(dT * 1000);
     Timer1.attachInterrupt(time_isr);
     
     Uart.begin(57600);
+    
+    FC.begin(4800);
     
     Serial.println("start");
     Serial.print("dp offset = ");
@@ -239,22 +306,38 @@ void setup()
     Serial.println("V");
 }
 
+int gps_messages = 0;
+String last_gps = String();
 
 void loop()
 {
-    int i, value;
+    int i;
     long since_last_copy;
     float climb_rate, climb_rate_filter, airspeed;
+    
 
     if (Uart.available()) {
 	char c = Uart.read();
-// 	    Serial.print(c);  // uncomment to see raw GPS data
+ 	 //   Serial.print(c);  // uncomment to see raw GPS data
+	if (c == '$')
+	    gps_s = String() + '$';
+	else
+	    gps_s = gps_s + c;
 	if (gps.encode(c)) {
 	    long lat, lon;
 	    unsigned long age;
 	    gps.get_position(&lat, &lon, &age);
-	    Serial.print("Lat/Long(10^-5 deg): "); Serial.print(lat); Serial.print(", "); Serial.print(lon); 
-	    Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
+	    //Serial.print("Lat/Long(10^-5 deg): "); Serial.print(lat); Serial.print(", "); Serial.print(lon); 
+	    //Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
+	    /*
+	    if (gps_s.startsWith("$GPGGA")) {		  
+		    Serial.println(gps_s);
+	    }*/
+	    //Serial.println(gps_s);
+	    if (gps_s.startsWith("$GPRMC"))
+// 	    if (gps_s.startsWith("$GPGGA"))
+		last_gps = gps_s;
+	    gps_s = String();
 	}
     }
     if (grab_data) {
@@ -284,11 +367,13 @@ void loop()
 	
 	//climb_rate = lrCoef[0] * 1000; // in m/sec
 	climb_rate = climb_rate_filter;
-	
+
+#ifdef USE_STEPPER
 	old_position = position;
 	position = constrain(300 - int(climb_rate * STEP_MULT), 0, 600);
 	stepper.step(position - old_position);
-
+#endif //USE_STEPPER
+	
 	// NOTE: can do non linear function here, e.g. logarithmic
 	// scaled so 5/msec is full range
 	toneFreq = constrain(climb_rate * 100, -500, 500);
@@ -302,7 +387,7 @@ void loop()
 	    ddsAcc = 0;
 	if (ddsAcc < 0)
 	    ddsAcc = 0;
-
+/*
 	Serial.print("i= ");
 	Serial.print(counter);
 	Serial.print(", dt (ms)= ");
@@ -331,12 +416,20 @@ void loop()
 	Serial.print(", V= ");
 	Serial.print(airspeed);
 	Serial.println(" m/sec");
+*/
 	// using the linear regression instead of my stupidity
 	//if (climb_rate < neg_dead_band || ( climb_rate > pos_dead_band && ddsAcc > sound_threshold))
 	if (climb_rate < neg_dead_band || ( climb_rate > pos_dead_band && counter % 12 > 6))
 	    tone(speaker_PIN, toneFreq + 510);
 	else
 	    noTone(speaker_PIN);
+	
+	// send info every 1sec
+	if (counter % 25 == 0) {
+	    info2FC(airspeed, climb_rate_filter, pressure);
+	    Serial.println(last_gps);
+	    FC.println(last_gps);
+	}
 	counter++;
     }
 }
